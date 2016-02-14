69a6bb8451fd905b8f46daa40f7485ef79030344
mantvius <mantvius@gmail.com>
2016-02-14 14:08:43 +0200

    some tweaking

--- growth_plot_modified.py
+++ growth_plot_modified.py
@@ -26,15 +26,33 @@ def f(n):
     """
     A test function
     """
-    return 0.1 * n ** 3 + 20 * n
+    #return 0.1 * n ** 3 + 20 * n
+    return 1000 * n
 
 def g(n):
     """
     A test function
     """
-    return n ** 3
+    #return n ** 3
     #return 20 * n ** 2
     #return .1 * n ** 4
+    return (n-1) * 1.15
+
+
+def make_plot(fun1, fun2, plot_length):
+    """
+    Create a plot relating the growth of fun1 vs. fun2
+    """
+    answer = []
+    for index in range(10, plot_length):
+        answer.append([index, fun1(index) / float(fun2(index))])
+    simpleplot.plot_lines("Growth rate comparison", 300, 300, "n", "f(n)/g(n)", [answer])
+
+# create an example plot
+make_plot(f, g, 1000)
+
+
+
 
 
 def greedy_boss(days_in_simulation, bribe_cost_increment, plot_type = STANDARD):
@@ -95,22 +113,22 @@ def boss(days):
     return greedy_boss(days, 0, LOGLOG)[1]
 
 
-def make_plot(fun1, fun2, plot_length):
-    """
-    Create a plot relating the growth of fun1 vs. fun2
-    """
-    answer = []
-    points = [[2.30258509299, 6.90775527898], [2.7080502011, 7.60090245954], [2.94443897917, 8.07090608879], [3.04452243772, 8.2940496401], [3.13549421593, 8.51719319142], [3.21887582487, 8.73230457103], [3.295836866, 8.93590352627], [3.33220451018, 9.03598698483], [3.36729582999, 9.13776967914], [3.40119738166, 9.23989917422], [3.43398720449, 9.34136863438], [3.4657359028, 9.44145209294], [3.49650756147, 9.53964411912], [3.52636052462, 9.63560810738], [3.52636052462, 9.63560810738], [3.55534806149, 9.73506890091]]
-
-    for point in points:
-        answer.append([point[0], point[1] / float(fun2(point[0]))])
-    simpleplot.plot_lines("Growth rate comparison", 300, 300, "n", "boss(n)/c(n)", [answer])
-
-    #simpleplot.plot_lines("Greedy boss", 600, 600, "days", "total earnings", [inc_0])
-
-
-# create an example plot    
-make_plot(boss, c, 100)
+# def make_plot(fun1, fun2, plot_length):
+#     """
+#     Create a plot relating the growth of fun1 vs. fun2
+#     """
+#     answer = []
+#     points = [[2.30258509299, 6.90775527898], [2.7080502011, 7.60090245954], [2.94443897917, 8.07090608879], [3.04452243772, 8.2940496401], [3.13549421593, 8.51719319142], [3.21887582487, 8.73230457103], [3.295836866, 8.93590352627], [3.33220451018, 9.03598698483], [3.36729582999, 9.13776967914], [3.40119738166, 9.23989917422], [3.43398720449, 9.34136863438], [3.4657359028, 9.44145209294], [3.49650756147, 9.53964411912], [3.52636052462, 9.63560810738], [3.52636052462, 9.63560810738], [3.55534806149, 9.73506890091]]
+#
+#     for point in points:
+#         answer.append([point[0], point[1] / float(fun2(point[0]))])
+#     simpleplot.plot_lines("Growth rate comparison", 300, 300, "n", "boss(n)/c(n)", [answer])
+#
+#     #simpleplot.plot_lines("Greedy boss", 600, 600, "days", "total earnings", [inc_0])
+#
+#
+# # create an example plot
+# make_plot(boss, c, 100)
 
 if SIMPLEGUICS2PYGAME:
     simpleplot._block()
--- poc_greedy_solution.py
+++ poc_greedy_solution.py
@@ -7,6 +7,17 @@ import math
 # import codeskulptor
 # codeskulptor.set_timeout(20)
 
+try:
+    #import simplegui
+    import simpleplot
+    SIMPLEGUICS2PYGAME = False
+
+except ImportError:
+    #import SimpleGUICS2Pygame.simplegui as simplegui
+    import SimpleGUICS2Pygame.simpleplot as simpleplot
+    SIMPLEGUICS2PYGAME = True
+
+
 STANDARD = True
 LOGLOG = False
 
@@ -39,6 +50,7 @@ def greedy_boss(days_in_simulation, bribe_cost_increment, plot_type = STANDARD):
         if plot_type == STANDARD:
             days_vs_earnings.append((current_day, total_salary_earned))
         else:
+            print current_day, total_salary_earned
             days_vs_earnings.append([math.log(current_day), math.log(total_salary_earned)])
         
         # check whether we have enough money to bribe without waiting
@@ -61,27 +73,27 @@ def greedy_boss(days_in_simulation, bribe_cost_increment, plot_type = STANDARD):
     return days_vs_earnings
 
 
-# def run_simulations():
-#     """
-#     Run simulations for several possible bribe increments
-#     """
-#     plot_type = STANDARD
-#     days = 70
-#     inc_0 = greedy_boss(days, 0, plot_type)
-#     inc_500 = greedy_boss(days, 500, plot_type)
-#     inc_1000 = greedy_boss(days, 1000, plot_type)
-#     inc_2000 = greedy_boss(days, 2000, plot_type)
-#     simpleplot.plot_lines("Greedy boss", 600, 600, "days", "total earnings",
-#                           [inc_0, inc_500, inc_1000, inc_2000], False,
-#                          ["Bribe increment = 0", "Bribe increment = 500",
-#                           "Bribe increment = 1000", "Bribe increment = 2000"])
+def run_simulations():
+    """
+    Run simulations for several possible bribe increments
+    """
+    plot_type = LOGLOG
+    days = 70
+    inc_0 = greedy_boss(days, 0, plot_type)
+    inc_500 = greedy_boss(days, 500, plot_type)
+    inc_1000 = greedy_boss(days, 1000, plot_type)
+    inc_2000 = greedy_boss(days, 2000, plot_type)
+    simpleplot.plot_lines("Greedy boss", 600, 600, "days", "total earnings",
+                          [inc_0, inc_500, inc_1000, inc_2000], False,
+                         ["Bribe increment = 0", "Bribe increment = 500",
+                          "Bribe increment = 1000", "Bribe increment = 2000"])
 
-# run_simulations()
+run_simulations()
 
-print greedy_boss(35, 100)
+# print greedy_boss(35, 100)
 # should print [(0, 0), (10, 1000), (16, 2200), (20, 3400), (23, 4600), (26, 6100), (29, 7900), (31, 9300), (33, 10900), (35, 12700)]
 
-print greedy_boss(35, 0)
+# print greedy_boss(35, 0)
 # should print [(0, 0), (10, 1000), (15, 2000), (19, 3200), (21, 4000), (23, 5000), (25, 6200), (27, 7600), (28, 8400), (29, 9300), (30, 10300), (31, 11400), (32, 12600), (33, 13900), (34, 15300), (34, 15300), (35, 16900)]
-            
-    
+
+print greedy_boss(70, 0, LOGLOG)
